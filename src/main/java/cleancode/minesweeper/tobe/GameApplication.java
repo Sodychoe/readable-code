8ddf1c7e9bd8c41c9b98682f6b9cf7f63cb45f3b
sodychoe <sodychoe@icloud.com>
2024-10-07 02:22:48 +0900

    섹션4- OCP
    요구사항 추가(게임 난이도를 변경할 수 있게 하기)에 따라 유연한 확장을 가능하게 하도록 인터페이스를 작성하고 기존에 상수였던 값들을 인터페이스 구현체가 설정할 수 있도록 변경, 입출력 로직도 확장에 맞게 변경

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,8 +1,13 @@
 package cleancode.minesweeper.tobe;
 
+import cleancode.minesweeper.tobe.gameLevel.Begineer;
+import cleancode.minesweeper.tobe.gameLevel.GameLevel;
+
 public class GameApplication {
     public static void main(String[] args) {
-        Minesweeper minesweeper = new Minesweeper();
+        GameLevel gameLevel = new Begineer();
+
+        Minesweeper minesweeper = new Minesweeper(gameLevel);
         minesweeper.run();
     }
 }
--- src/main/java/cleancode/minesweeper/tobe/GameBoard.java
+++ src/main/java/cleancode/minesweeper/tobe/GameBoard.java
@@ -1,15 +1,20 @@
 package cleancode.minesweeper.tobe;
 
+import cleancode.minesweeper.tobe.gameLevel.GameLevel;
 import java.util.Arrays;
 import java.util.Random;
 
 public class GameBoard {
 
-  private static final int LAND_MINE_COUNT = 10;
   private final Cell[][] board;
+  private final int landMineCount;
 
-  public GameBoard(int rowSize, int colSize) {
+  public GameBoard(GameLevel gameLevel) {
+    int rowSize = gameLevel.getRowSize();
+    int colSize = gameLevel.getColSize();
     board = new Cell[rowSize][colSize];
+
+    this.landMineCount = gameLevel.getLandMineCount();
   }
 
   public void initializeGame() {
@@ -21,7 +26,7 @@ public class GameBoard {
         board[row][col] = Cell.create();
       }
     }
-    for (int i = 0; i < LAND_MINE_COUNT; i++) {
+    for (int i = 0; i < landMineCount; i++) {
       int landMineCol = new Random().nextInt(colSIze);
       int landMineRow = new Random().nextInt(rowSIze);
       Cell landMineCell = findCell(landMineRow, landMineCol);
