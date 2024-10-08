984addd76e03f3f3e557151fd59d714dfd1f9b8e
sodychoe <sodychoe@icloud.com>
2024-10-08 18:08:22 +0900

    섹션5- 다형성
    CellSignProvider 인터페이스는 주어진 cellStatus 를 보고 출력을 하는 객체인데 구현체가 추가될때마다 if문에 개발자가 수동으로 추가해줘야 하는 단점이 있었음 이를 다형성을 활용하여 if문을 제거하고 enum을 활용하여 구현체를 수동으로 추가해야 하는 부분을 개선함

--- src/main/java/cleancode/minesweeper/tobe/io/ConsoleOutputHandler.java
+++ src/main/java/cleancode/minesweeper/tobe/io/ConsoleOutputHandler.java
@@ -3,20 +3,13 @@ package cleancode.minesweeper.tobe.io;
 import cleancode.minesweeper.tobe.GameBoard;
 import cleancode.minesweeper.tobe.GameException;
 import cleancode.minesweeper.tobe.cell.CellSnapshot;
-import cleancode.minesweeper.tobe.cell.CellSnapshotStatus;
+import cleancode.minesweeper.tobe.io.sign.CellSignProvider;
 import cleancode.minesweeper.tobe.position.CellPosition;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.IntStream;
 
 public class ConsoleOutputHandler implements OutputHandler{
-  private static final String EMPTY_SIGN = "■";
-  private static final String LAND_MINE_SIGN = "☼";
-  private static String UNCHECKED_SIGN = "□";
-  private static String FLAG_SIGN = "⚑";
-
-
-
   @Override
   public void showGameStartComments() {
     System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
@@ -33,35 +26,12 @@ public class ConsoleOutputHandler implements OutputHandler{
       for (int col = 0; col < board.getColSize(); col++) {
         CellPosition cellPosition = CellPosition.of(row, col);
         CellSnapshot cellSnapshot = board.getSnapshot(cellPosition);
-        String cellSign = decideCellSignFrom(cellSnapshot);
-
+        String cellSign = CellSignProvider.findCellSignFrom(cellSnapshot);
 
         System.out.print(cellSign + " ");
       }
       System.out.println();
     }
-  }
-
-  private String decideCellSignFrom(CellSnapshot cellSnapshot) {
-    CellSnapshotStatus status = cellSnapshot.getStatus();
-    if (status == CellSnapshotStatus.EMPTY) {
-      return EMPTY_SIGN;
-    }
-    if (status == CellSnapshotStatus.FLAG) {
-      return FLAG_SIGN;
-    }
-    if (status == CellSnapshotStatus.LAND_MINE) {
-      return LAND_MINE_SIGN
-    }
-    if (status == CellSnapshotStatus.NUMBER) {
-      return String.valueOf(cellSnapshot.getNearbyLandMineCount());
-    }
-
-    if (status == CellSnapshotStatus.UNCHECKED) {
-      return UNCHECKED_SIGN
-    }
-
-    throw new IllegalArgumentException("확인할 수 없는 셀입니다");
   }
 
   private String generateColAlphabets(GameBoard board) {
